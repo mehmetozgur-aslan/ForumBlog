a683965b394dc40c5b76e74f314e83815b05678e
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-02 15:31:20 +0300

    Blog DTO ve Blog Model classları oluşturuldu

---
+++ ForumBlog.DTO/DTOs/BlogDtos/BlogListDto.cs
@@ -0,0 +1,17 @@
+﻿using ForumBlog.DTO.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumBlog.DTO.DTOs.BlogDtos
+{
+    public class BlogListDto : IDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime PostedTime { get; set; }
+    }
+}
---
+++ ForumBlog.DTO/Interface/IDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumBlog.DTO.Interface
+{
+    public interface IDTO
+    {
+    }
+}
