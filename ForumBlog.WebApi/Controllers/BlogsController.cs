b0000a38ee94ed14cace061ae4ca23626fb3a2e2
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-02 15:19:52 +0300

    BlogsController taslak metotları yazıldı.

---
+++ ForumBlog.WebApi/Controllers/BlogsController.cs
@@ -0,0 +1,64 @@
+﻿using ForumBlog.Business.Interface;
+using ForumBlog.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+        public BlogsController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _blogService.GetAllSortedByPostedTimeAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _blogService.FindByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Blog blog)
+        {
+            await _blogService.AddAsync(blog);
+
+            return Created("", blog);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Blog blog)
+        {
+            if (id != blog.Id)
+            {
+                return BadRequest("geçersiz id");
+            }
+
+            await _blogService.UpdateAsync(blog);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _blogService.RemoveAsync(new Blog { Id = id });
+
+            return NoContent();
+        }
+
+    }
+}
