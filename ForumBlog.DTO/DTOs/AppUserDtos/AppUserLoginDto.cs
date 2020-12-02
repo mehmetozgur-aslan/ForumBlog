a044720f4df84774cd9f208d7b8fbfaea931b8f7
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-02 15:37:50 +0300

    AppUserLoginDto oluşturuldu

---
+++ ForumBlog.DTO/DTOs/AppUserDtos/AppUserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using ForumBlog.DTO.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumBlog.DTO.DTOs.AppUserDtos
+{
+    public class AppUserLoginDto : IDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
