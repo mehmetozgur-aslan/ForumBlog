d4674b00e02fd6b640043f273317b0df929f229a
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-03 18:47:54 +0300

    BaseController yazıldı

---
+++ ForumBlog.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,48 @@
+﻿using ForumBlog.WebApi.Enums;
+using ForumBlog.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public async Task<UploadModel> UploadFile(IFormFile file, string contentType)
+        {
+            UploadModel uploadModel = new UploadModel();
+
+            if (file != null)
+            {
+                if (file.ContentType != contentType)
+                {
+                    uploadModel.ErrorMessage = "Uygunsuz dosya türü";
+                    uploadModel.UploadState = UploadState.Error;
+                    return uploadModel;
+                }
+                else
+                {
+                    var newName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img" + newName);
+
+                    var stream = new FileStream(path, FileMode.Create);
+                    await file.CopyToAsync(stream);
+
+                    uploadModel.NewName = newName;
+                    uploadModel.UploadState = UploadState.Success;
+                    return uploadModel;
+                }
+            }
+
+            uploadModel.ErrorMessage = "Dosya yok";
+            uploadModel.UploadState = UploadState.NotExist;
+            return uploadModel;
+        }
+    }
+}
