9f5ad5e3330d46b4f23a983c891ddacc7dfdd401
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-03 15:30:21 +0300

    AutoMapper ekledi

---
+++ ForumBlog.WebApi/Mapping/AutoMapper/MapProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ForumBlog.DTO.DTOs.BlogDtos;
+using ForumBlog.DTO.DTOs.CategoryDtos;
+using ForumBlog.Entities.Concrete;
+using ForumBlog.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumBlog.WebApi.Mapping.AutoMapper
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<BlogListDto, Blog>();
+            CreateMap<Blog, BlogListDto>();
+
+            CreateMap<BlogUpdateModel, Blog>();
+            CreateMap<Blog, BlogUpdateModel>();
+
+            CreateMap<Blog, BlogAddModel>();
+            CreateMap<BlogAddModel, Blog>();
+
+            CreateMap<CategoryAddDto, Category>();
+            CreateMap<Category, CategoryAddDto>();
+
+            CreateMap<CategoryListDto, Category>();
+            CreateMap<Category, CategoryListDto>();
+
+            CreateMap<Category, CategoryUpdateDto>();
+            CreateMap<CategoryUpdateDto, Category>();
+        }
+    }
+}
