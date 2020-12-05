99a7e49f2065caf3097563641f0b5c6db648c645
Asus <Asus@DESKTOP-7QSBH7L>
2020-12-05 14:45:31 +0300

    Startup classında Jwt konfigurasyonu

---
+++ ForumBlog.Business/StringInfos/JwtInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumBlog.Business.StringInfos
+{
+   public class JwtInfo
+    {
+        public const string Issuer = "http://localhost:54683";
+        public const string Audience = "http://localhost:5000";
+        public const string SecurityKey = "mehmetozguraslan1";
+    }
+}
--- ForumBlog.WebApi/Startup.cs
+++ ForumBlog.WebApi/Startup.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using ForumBlog.Business.Containers.MicrosoftIoC;
+using ForumBlog.Business.StringInfos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -7,9 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ForumBlog.WebApi
@@ -28,6 +32,22 @@ namespace ForumBlog.WebApi
         {
             services.AddAutoMapper(typeof(Startup));
             services.AddDepencencies();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidIssuer = JwtInfo.Issuer,
+                    ValidAudience = JwtInfo.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtInfo.SecurityKey)),
+                    ValidateLifetime = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ClockSkew = TimeSpan.Zero // Sunucular arasý zaman farký koyma
+                };
+            });
+
             services.AddControllers().AddNewtonsoftJson(opt =>
             {
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
